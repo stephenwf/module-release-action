43260c63b04f4707092f15f52b92d6b6bd39f9f4
stephenwf <stephen.fraser@digirati.com>
2019-02-17 21:38:51 +0000

    Debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,8 +12,13 @@ if [[ -z "$NPM_AUTH" ]]; then
   exit 1
 fi
 
-number=$(jq --raw-output .pull_request.number "$GITHUB_EVENT_PATH")
+GH_ACTION=$(jq --raw-output .action "$GITHUB_EVENT_PATH")
+GH_PR_NUMBER=$(jq --raw-output .pull_request.number "$GITHUB_EVENT_PATH")
 
+if [[ GH_ACTION != "synchronized" ]]; then
+    echo "Not required unless there is new code.";
+    exit 0;
+fi;
 
 IS_DEBUG_MODE_ENABLED=${MODULE_RELEASE_DEBUG:-false}  # If variable not set or null, use default.
 
@@ -21,13 +26,13 @@ IS_DEBUG_MODE_ENABLED=${MODULE_RELEASE_DEBUG:-false}  # If variable not set or n
 if [[ "$IS_DEBUG_MODE_ENABLED" != false ]]; then
     debugEventPath=$(jq "." "$GITHUB_EVENT_PATH")
 
-    echo " DEBUG: Pull request number $number";
+    echo " DEBUG: Pull request number $GH_PR_NUMBER";
     echo " DEBUG: Github event path $debugEventPath"
 fi;
 
 echo "//registry.npmjs.org/:_authToken=${NPM_AUTH}" >> ~/.npmrc
 
-if [[ "$GITHUB_REF" = "refs/heads/master" ]] && [[ "$number" = "null" ]]; then
+if [[ "$GITHUB_REF" = "refs/heads/master" ]] && [[ "$GH_PR_NUMBER" = "null" ]]; then
     echo "======================================================";
     echo "  Deploying a canary release";
     echo "======================================================";
@@ -44,11 +49,11 @@ fi;
 #fi;
 
 
-if [[ "$number" != "null" ]]; then
+if [[ "$GH_PR_NUMBER" != "null" ]]; then
     echo "======================================================";
-    echo "  Deploying pull request number: $number";
+    echo "  Deploying pull request number: $GH_PR_NUMBER";
     echo "======================================================";
     if [[ "$IS_DEBUG_MODE_ENABLED" = false ]]; then
-        fesk-release --pull-request=${number} --yes
+        fesk-release --pull-request=${GH_PR_NUMBER} --yes
     fi;
 fi;
